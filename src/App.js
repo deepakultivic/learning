daa4190d02fa1df748920cac3a0a029685bdf7fe
deepalultivic <deepak.chauhan@ultivic.com>
2021-05-04 16:58:49 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import './App.css';
+import {
+    BrowserRouter,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function About(){
+    return(
+        <>about works !</>
+    );
+}
+export default About;
